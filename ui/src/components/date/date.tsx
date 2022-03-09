f237862cebf08ec216deca93656ca0d8aa4b3158
sudharsan-gandhi <sudharsangandhi@gmail.com>
2022-03-10 00:43:55 +0530

    clear date field fixed

--- ui/src/components/date/date.tsx
+++ ui/src/components/date/date.tsx
@@ -1,19 +1,38 @@
+import { useRef } from 'react';
+
 import { SingleDatepicker } from 'chakra-dayzed-datepicker';
 import { useController } from 'react-hook-form';
+import { MdClear } from 'react-icons/md';
+
+import {
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
 
 export const KBDatePicker: React.FC<{
   control: any;
   label: string;
   defaultValue?: Date;
 }> = ({ control, label, defaultValue }) => {
+  const ref = useRef();
   const {
+    fieldState,
     field: { onChange, value },
   } = useController({
     name: label,
     control,
     defaultValue,
   });
+
   return (
-    <SingleDatepicker onDateChange={onChange} date={value} />
+    <InputGroup>
+      <SingleDatepicker onDateChange={onChange} date={value} />
+      <InputRightElement
+        children={<MdClear />}
+        onClick={() => {
+          onChange();
+        }}
+      />
+    </InputGroup>
   );
 };
